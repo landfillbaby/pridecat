4610e78febcaf4fb88b963219f3b9b8486739420
Lucy Phipps <landfillbaby69@gmail.com>
2022-10-01 05:08:20 +0100

    code formatting

--- pridebg-lite-trans-bi.c
+++ pridebg-lite-trans-bi.c
@@ -11,58 +11,67 @@ n = 2: clear whole line
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define x() if(l != 19) fputs("\033[39;49m\033[K", stdout)
-#define rgb(r, g, b) fputs("\033[48;2;" #r ";" #g ";" #b "m\033[K", stdout)
+#define x() \
+  if(l != 19) fputs("\33[39;49m\33[K", stdout)
+#define rgb(r, g, b) fputs("\33[48;2;" #r ";" #g ";" #b "m\33[K", stdout)
 static unsigned char l, p;
-static void cat(FILE *f){
+static void cat(FILE *f) {
   int c = getc(f);
   if(c < 0) return;
-  if(l == 19){
-    fputs("\033[38;5;16m\033[48;2;91;206;250m\033[K", stdout); // black text
+  if(l == 19) {
+    fputs("\33[38;5;16m\33[48;2;91;206;250m\33[K", stdout); // black text
     l = 0;
     goto uhh;
   }
-  do{
-    if(p == '\n'){
-      switch(l = (l + 1) % 10){
-	case 0: case 4: rgb(91, 206, 250); break;
-	case 1: case 3: rgb(245, 169, 184); break;
-	case 2: fputs("\033[48;5;231m\033[K", stdout); break;
+  do {
+    if(p == '\n') switch(l = (l + 1) % 10) {
+	case 0:
+	case 4: rgb(91, 206, 250); break;
+	case 1:
+	case 3: rgb(245, 169, 184); break;
+	case 2: fputs("\33[48;5;231m\33[K", stdout); break;
 	case 5:
 #ifdef DUPECOLORS
 	case 6:
 #endif
-		rgb(214, 2, 112); break;
+	  rgb(214, 2, 112);
+	  break;
 	case 7: rgb(155, 79, 150); break;
 	case 8:
 #ifdef DUPECOLORS
 	case 9:
 #endif
-		rgb(0, 56, 168); break;
+	  rgb(0, 56, 168);
+	  break;
 #ifndef DUPECOLORS
-	case 6: case 9: fputs("\033[K", stdout);
+	case 6:
+	case 9: fputs("\33[K", stdout);
 #endif
-    } }
-uhh:
+      }
+  uhh:
     putc(p = c, stdout);
-  }while((c = getc(f)) >= 0);
+  } while((c = getc(f)) >= 0);
 }
-static void abrt(int signo){ x(); exit(signo); }
-int main(int c, char **v){
+static void abrt(int signo) {
+  x();
+  exit(signo);
+}
+int main(int c, char **v) {
   l = 19;
   signal(SIGINT, abrt);
   if(c < 2) cat(stdin);
-  else for(--c, ++v; c; --c, ++v){
-    if(**v == '-' && !v[0][1]) cat(stdin);
-    else{
-      FILE *f = fopen(*v, "r");
-      if(!f){
-	x();
-	fprintf(stderr, "pridebg: Couldn't open \"%s\" for reading.\n", *v);
-	return 1;
+  else
+    for(--c, ++v; c; --c, ++v)
+      if(**v == '-' && !v[0][1]) cat(stdin);
+      else {
+	FILE *f = fopen(*v, "r");
+	if(!f) {
+	  x();
+	  fprintf(stderr, "pridebg: Couldn't open \"%s\" for reading.\n", *v);
+	  return 1;
+	}
+	cat(f);
+	fclose(f);
       }
-      cat(f);
-      fclose(f);
-  } }
   x();
 }
--- pridecat-lite-trans-bi.c
+++ pridecat-lite-trans-bi.c
@@ -14,73 +14,84 @@ reimplement the printf of hexadecimal?
 #ifndef DUPECOLORS
 #define DUPECOLORS
 #endif
-#define ESC "\033[4"
-#define x() if(l != 19) fputs("\033[39;49m", stdout)
+#define ESC "\33[4"
+#define x() \
+  if(l != 19) fputs("\33[39;49m", stdout)
 #else
-#define ESC "\033[3"
-#define x() if(l != 19) fputs(ESC "9m", stdout)
+#define ESC "\33[3"
+#define x() \
+  if(l != 19) fputs(ESC "9m", stdout)
 #endif
 #define rgb(r, g, b) fputs(ESC "8;2;" #r ";" #g ";" #b "m", stdout)
 static unsigned char l;
 static bool e;
-static void cat(FILE *f){
+static void cat(FILE *f) {
   int c;
 #define C(x) (c != x)
-  while((c = getc(f)) >= 0){
-    if(!C('\n')){
+  while((c = getc(f)) >= 0) {
+    if(!C('\n')) {
 #ifdef PRIDEHL
       if(l != 19) fputs(ESC "9m", stdout);
 #endif
       e = false;
-    }else if(!e && C(' ') && C('\t') && C('\r') && C('\f') && C('\v')){
+    } else if(!e && C(' ') && C('\t') && C('\r') && C('\f') && C('\v')) {
 #ifdef PRIDEHL
-      if(l == 19){
+      if(l == 19) {
 	l = 0;
-	fputs("\033[38;5;16m\033[48;2;91;206;250m", stdout); // black text
-      }else
+	fputs("\33[38;5;16m\33[48;2;91;206;250m", stdout); // black text
+      } else
 #endif
-      switch(l = (l + 1) % 10){
-	case 0: case 4: rgb(91, 206, 250); break;
-	case 1: case 3: rgb(245, 169, 184); break;
-	case 2: /*rgb(255, 255, 255);*/ fputs(ESC "8;5;231m", stdout); break;
-	case 5:
+	switch(l = (l + 1) % 10) {
+	  case 0:
+	  case 4: rgb(91, 206, 250); break;
+	  case 1:
+	  case 3: rgb(245, 169, 184); break;
+	  case 2: /*rgb(255, 255, 255);*/ fputs(ESC "8;5;231m", stdout); break;
+	  case 5:
 #ifdef DUPECOLORS
-	case 6:
+	  case 6:
 #endif
-	  rgb(214, 2, 112); break;
-	case 7: rgb(155, 79, 150); break;
-	case 8:
+	    rgb(214, 2, 112);
+	    break;
+	  case 7: rgb(155, 79, 150); break;
+	  case 8:
 #ifdef DUPECOLORS
-	case 9:
+	  case 9:
 #endif
-	  rgb(0, 56, 168);
-      }
+	    rgb(0, 56, 168);
+	}
       e = true;
     }
     putc(c, stdout);
-} }
-static void abrt(int signo){ x(); exit(signo); }
-int main(int c, char **v){
+  }
+}
+static void abrt(int signo) {
+  x();
+  exit(signo);
+}
+int main(int c, char **v) {
   l = 19;
   signal(SIGINT, abrt);
   if(c < 2) cat(stdin);
-  else for(--c, ++v; c; --c, ++v){
-    if(**v == '-' && !v[0][1]) cat(stdin);
-    else{
-      FILE *f = fopen(*v, "r");
-      if(!f){
-	x();
-	fprintf(stderr, "pride"
+  else
+    for(--c, ++v; c; --c, ++v)
+      if(**v == '-' && !v[0][1]) cat(stdin);
+      else {
+	FILE *f = fopen(*v, "r");
+	if(!f) {
+	  x();
+	  fprintf(stderr,
 #ifdef PRIDEHL
-	    "hl"
+	    "pridehl"
 #else
-	    "cat"
+	    "pridecat"
 #endif
-	    ": Couldn't open \"%s\" for reading.\n", *v);
-	return 1;
+	    ": Couldn't open \"%s\" for reading.\n",
+	    *v);
+	  return 1;
+	}
+	cat(f);
+	fclose(f);
       }
-      cat(f);
-      fclose(f);
-  } }
   x();
 }
